bce3987a8983a6b92d91f0cca55d8aa96d299079
heejun <heejjuunn@gmail.com>
2022-03-31 09:17:12 +0900

    0331: 알고리즘시작 전

--- webEx/src/ws0330/BOJ1600_말이되고픈원숭이.java
+++ webEx/src/ws0330/BOJ1600_말이되고픈원숭이.java
@@ -3,6 +3,8 @@ package ws0330;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1600_말이되고픈원숭이 {
@@ -23,6 +25,9 @@ public class BOJ1600_말이되고픈원숭이 {
 	
 	static int K,W,H;
 	static int[][] mat;
+	static int[] dx= {1,-1,0,0}, dy= {0,0,1,-1};
+	static int[] hx = {1,2,2,1,-1,-2,-2,-1};
+	static int[] hy = {2,1,-1,-2,-2,-1,1,2};
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		K = Integer.parseInt(br.readLine());
@@ -38,6 +43,53 @@ public class BOJ1600_말이되고픈원숭이 {
 			}
 		}//...입력완료
 		
+		System.out.println(bfs(0,0));
+	}
+	static int bfs(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		boolean[][][] check = new boolean[H][W][K+1];
 		
+		check[y][x][0] = true;
+		q.add(new int[] {x,y,0,0});
+		while(!q.isEmpty()) {
+			int[] pos = q.poll();
+			int px = pos[0], py = pos[1];
+			int chance = pos[2], move=pos[3];
+			
+			if(px==W-1 && py==H-1) {
+				 return move;
+			}
+			
+			//기본이동
+			for(int i=0; i<4; i++) {
+				int nx = px + dx[i];
+				int ny = py + dy[i];
+				
+				if(nx<0 || ny>W-1 || ny<0 || ny>H-1) continue;
+				if(check[ny][nx][chance]) continue;
+				
+				if(mat[ny][nx] !=1) {
+					check[ny][nx][chance] = true;
+					q.add(new int[] {nx,ny,chance, move+1});
+				}
+			}
+			
+			//말 찬스이동: 참스 남은 경우 8방탐색
+			if(chance<K) {
+				for(int i=0; i<8; i++) {
+					int nx = px + hx[i];
+					int ny = py + hy[i];
+					
+					if(nx<0 || nx>W-1 || ny<0 || ny>H-1) continue;
+					if(check[ny][nx][chance+1])  continue;
+					
+					if(mat[ny][nx] != 1) {
+						check[ny][nx][chance+1] = true;
+						q.add(new int[] {nx,ny,chance+1, move+1});
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
